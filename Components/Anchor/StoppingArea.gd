aabf11ddfc46a5a247558e9aad3ec448be172bc4
slow chaz <corfitzgerald@gmail.com>
2025-04-05 14:14:09 -0400

    platform & anchor collision detections and platform health

---
+++ Components/Anchor/StoppingArea.gd
@@ -0,0 +1,13 @@
+extends Area3D
+
+func _ready() -> void:
+	if not is_connected("body_entered", _on_body_entered):
+		connect("body_entered", _on_body_entered)
+
+func _on_body_entered(body: Node3D) -> void:
+	if body is StaticBody3D:
+		var mesh_instance = body.get_parent()
+		if mesh_instance is MeshInstance3D:
+			var platform = mesh_instance.get_parent()
+			if platform is SpawnableEntity:
+				platform.take_damage(1)
