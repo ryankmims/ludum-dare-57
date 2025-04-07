7501dc4185ac17881254066e6b78e9bb57d1afae
slow chaz <corfitzgerald@gmail.com>
2025-04-07 16:31:51 -0400

    fixing git mistakes

--- Scripts/Components/SpawnableEntities/SpawnableEntity.gd
+++ Scripts/Components/SpawnableEntities/SpawnableEntity.gd
@@ -17,13 +17,33 @@ const LEVEL_TO_START_SPAWNING := 25
 
 @export var player : Player
 
-# Called when the node enters the scene tree for the first time.
+const DAMAGE_PARTICLE_SETTINGS = {
+	"amount": 20,
+	"explosiveness": 0.8,
+	"lifetime": 0.5,
+	"one_shot": true
+}
+
+const BREAK_PARTICLE_SETTINGS = {
+	"amount": 80,
+	"explosiveness": 0.9,
+	"lifetime": 1.5,
+	"speed_scale": 2.0,
+	"one_shot": true
+}
+
+const BREAK_MATERIAL_SETTINGS = {
+	"initial_velocity_min": 4.0,
+	"initial_velocity_max": 10.0,
+	"scale_min": 1.0,
+	"scale_max": 1.5
+}
+
 func _ready() -> void:
 	if particles_collection:
 		particles_collection.visible = false
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if global_position.y >= 25.0:
 		queue_free()
@@ -31,19 +51,13 @@ func _process(delta: float) -> void:
 	if environment_spawner.is_falling:
 		global_position.y += environment_spawner.float_speed * delta
 
-#func _on_collision_area_body_entered(body: Node3D) -> void:
-	#if body.name == "StaticBody3D" and body.get_parent().name == "Anchor":
-		#take_damage(1)
 		
 func take_damage(amount: int) -> void:
 	health -= amount
 	print("platform took damage!")
 	
 	shake_platform()
-	
-	if is_instance_valid(particles_collection):
-		print_debug("should turn visible")
-		particles_collection.visible = true
+	_emit_particles(DAMAGE_PARTICLE_SETTINGS)
 	
 	if health <= 0:
 		break_platform()
@@ -73,29 +87,18 @@ func break_platform() -> void:
 	sound_player_drop_instance.global_position = global_position
 	
 	if is_instance_valid(particles_collection):
-		# Make a copy of particles to keep them after platform is destroyed
 		var particles_instance = particles_collection.duplicate()
 		get_tree().root.add_child(particles_instance)
 		particles_instance.global_position = global_position
 		particles_instance.visible = true
 		
-		# Emit from all particle systems with dramatic settings
+		# Emit from all particle systems
 		for particle in particles_instance.get_children():
 			if particle is GPUParticles3D:
+				_configure_particles(particle, BREAK_PARTICLE_SETTINGS)
 				particle.emitting = true
-				particle.amount = 80
-				particle.explosiveness = 0.9
-				particle.lifetime = 1.5
-				particle.speed_scale = 2.0
-				particle.one_shot = true
 				
-				if particle.process_material is ParticleProcessMaterial:
-					var mat = particle.process_material as ParticleProcessMaterial
-					mat.initial_velocity_min = 4.0
-					mat.initial_velocity_max = 10.0
-					mat.scale_min = 1.0
-					mat.scale_max = 1.5
-					
+		# Add extra explosion effect
 		if particles_instance.get_child_count() > 0 and particles_instance.get_child(0) is GPUParticles3D:
 			var explosion = particles_instance.get_child(0).duplicate()
 			particles_instance.add_child(explosion)
@@ -105,12 +108,30 @@ func break_platform() -> void:
 			explosion.lifetime = 1.0
 			explosion.one_shot = true
 		
-		# Set up a timer to free the particles after they're done
+		# Set up cleanup timer
 		var timer = Timer.new()
 		particles_instance.add_child(timer)
-		timer.wait_time = 2.5  # Duration for the explosion effect
+		timer.wait_time = 2.5
 		timer.one_shot = true
 		timer.timeout.connect(func(): particles_instance.queue_free())
 		timer.start()
 	
 	queue_free()
+
+func _configure_particles(particle: GPUParticles3D, settings: Dictionary) -> void:
+	for key in settings:
+		particle.set(key, settings[key])
+	
+	if particle.process_material is ParticleProcessMaterial and settings == BREAK_PARTICLE_SETTINGS:
+		var mat = particle.process_material as ParticleProcessMaterial
+		for key in BREAK_MATERIAL_SETTINGS:
+			mat.set(key, BREAK_MATERIAL_SETTINGS[key])
+
+func _emit_particles(settings: Dictionary) -> void:
+	if is_instance_valid(particles_collection):
+		particles_collection.visible = true
+		for particle in particles_collection.get_children():
+			if particle is GPUParticles3D:
+				particle.emitting = false
+				_configure_particles(particle, settings)
+				particle.emitting = true
